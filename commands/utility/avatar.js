c4fe33b8413ff5a1b668e6b68f7cdbd7a42b5846
Alexis Amadei <alexisamadei@gmail.com>
2023-03-31 00:22:50 +0200

    Add avatar slash command

---
+++ commands/utility/avatar.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('avatar')
+    .setDescription('Display the tagged user avatar.')
+    .addUserOption(option =>
+        option.setName('user')
+        .setDescription('The user\'s avatar to display')
+        .setRequired(true)),
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const avatarEmbed = new EmbedBuilder()
+            .setTitle(`${user.tag}'s avatar`)
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 512 }))
+            await interaction.reply({ embeds: [avatarEmbed] });
+    },
+};
