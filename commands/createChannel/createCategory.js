93197d300fa4e4d3fc89571094742c491fa783d4
Alexis Amadei <alexisamadei@gmail.com>
2023-04-22 20:18:44 +0200

    add create categroy

---
+++ commands/createChannel/createCategory.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('createcategory')
+    .setDescription('Create a category')
+    .addStringOption(option =>
+      option.setName('category-name')
+        .setDescription('The name of the category.')
+        .setRequired(true)),
+  async execute(interaction) {
+    const guild = interaction.guild;
+    let elementName = interaction.options.getString('category-name').toUpperCase();
+    const category = await guild.channels.create({
+      name: elementName,
+      type: 4,
+    });
+    await interaction.reply('Created category ' + elementName);
+  }
+}
