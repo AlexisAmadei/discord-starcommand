781c1d71c5c087a9b19a4bd0a148427313db7113
Alexis Amadei <alexisamadei@gmail.com>
2023-04-22 20:18:36 +0200

    update create text channel

---
+++ commands/createChannel/createTextChannel.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('createtextchannel')
+    .setDescription('Create a text channel')
+    .addStringOption(option =>
+      option.setName('channel-name')
+        .setDescription('The name of the element.')
+        .setRequired(true))
+    .addStringOption(option =>
+      option.setName('topic')
+        .setDescription('The topic of the element.')
+        .setRequired(false)),
+  async execute(interaction) {
+    const guild = interaction.guild;
+    let elementName = interaction.options.getString('channel-name');
+    elementName = elementName.toLowerCase().replace(/[^a-zA-Z\s]+/g, '').replace(/\s+/g, '-');
+    const channel = await guild.channels.create({
+      name: elementName,
+      type: 0,
+    });
+    await interaction.reply(`Created channel ${channel}`);
+  }
+}
