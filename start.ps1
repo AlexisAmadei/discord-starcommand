cbb25d6364c75d49d8dfd46fb629d22bfd78ff60
root <root@starcommand>
2025-03-24 19:40:46 +0000

    delete ps1 startup script

--- start.ps1
+++
@@ -1,8 +0,0 @@
-# Run the first command
-node ./deploy-commands.js
-
-# Wait for the first command to finish executing before running the second command
-Start-Sleep -Seconds 2
-
-# Run the second command
-node .
